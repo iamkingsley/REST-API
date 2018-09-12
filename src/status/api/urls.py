7e425cd904c5a97e08964e7fc5394848c9690dcf
iamkingsley <cudjoebernard2@gmail.com>
2018-09-12 19:29:58 +0000

    Detail API View

--- src/status/api/urls.py
+++ src/status/api/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls import url
 
-from .views import StatusAPIView, StatusCreateAPIView
+from .views import (
+    StatusAPIView, 
+    StatusCreateAPIView,
+    StatusDetailAPIView,
+    )
 
 urlpatterns = [
     # url(r'^$', StatusListSearchAPIView.as_view()),
     url(r'^$', StatusAPIView.as_view()),
     url(r'^create/$', StatusCreateAPIView.as_view()),
-    # url(r'^(?P<id>.*)/$', StatusDetailAPIView.as_view()),
-    # url(r'^(?P<id>.*)/update/$', StatusUpdateAPIView.as_view()),
-    # url(r'^(?P<id>.*)/delete/$', StatusDeleteAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/$', StatusDetailAPIView.as_view()),
+    # url(r'^(?P<pk>\d+)/update/$', StatusUpdateAPIView.as_view()),
+    # url(r'^(?P<pk>\d+)/delete/$', StatusDeleteAPIView.as_view()),
     
 ]
 
