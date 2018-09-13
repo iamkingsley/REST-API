332eecbb6df68eb66e160d2de28c9f62c8916a64
iamkingsley <cudjoebernard2@gmail.com>
2018-09-13 22:38:06 +0000

    Implement JWT Authentication

--- src/api/urls.py
+++ src/api/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token # accounts app
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/auth/jwt/$', obtain_jwt_token),
+    url(r'^api/auth/jwt/refresh/$', refresh_jwt_token),
     url(r'^api/status/', include('status.api.urls')),
 ]
