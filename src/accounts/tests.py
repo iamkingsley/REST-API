77ae1507aa4230f4750daed375ea755ffcf5ad3d
iamkingsley <cudjoebernard2@gmail.com>
2018-09-16 23:16:35 +0000

    Automated Testing

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserTestCase(TestCase): 
+    def setUp(self): 
+        user =  User.objects.create(username='shirleycodjoe', email='shirleycodjoe@gmail.com')
+        user.set_password("Admin@123")
+        user.save()
+
+    def test_created_user(self):
+        qs = User.objects.filter(username='shirleycodjoe')
+        self.assertEqual(qs.count(), 1)
