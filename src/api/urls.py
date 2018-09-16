d48341e3df439323fda5c72741e5980b59ebfb5e
iamkingsley <cudjoebernard2@gmail.com>
2018-09-16 23:07:36 +0000

    Reverse URLs with DRF

--- src/api/urls.py
+++ src/api/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/auth/', include('accounts.api.urls')),
     url(r'^api/user/', include('accounts.api.user.urls', namespace='api-user')),
-    url(r'^api/status/', include('status.api.urls')),
+    url(r'^api/status/', include('status.api.urls', namespace='api-status')),
 ]
