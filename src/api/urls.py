3111bf6fa5f14e6333e691457a48b22e2a45e3c4
iamkingsley <cudjoebernard2@gmail.com>
2018-09-16 23:41:04 +0000

    Testing with a Temporary Image

--- src/api/urls.py
+++ src/api/urls.py
@@ -19,7 +19,7 @@ from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token # accou
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/auth/', include('accounts.api.urls', namespace='api-auth')),,
+    url(r'^api/auth/', include('accounts.api.urls', namespace='api-auth')),
     url(r'^api/user/', include('accounts.api.user.urls', namespace='api-user')),
     url(r'^api/status/', include('status.api.urls', namespace='api-status')),
 ]
